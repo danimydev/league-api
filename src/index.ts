b20c12521097cc60f0d29baa1156c80de8ec4756
Daniel Madrid <31113489+danimydev@users.noreply.github.com>
2023-07-01 10:03:00 -0500

    hello world at 3000

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,9 @@
-console.log("hello world");
+import { Application } from 'oak';
+
+const app = new Application();
+
+app.use((ctx) => {
+	ctx.response.body = 'hello world';
+});
+
+await app.listen({ port: 3000 });
