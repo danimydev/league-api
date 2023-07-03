d8356e3112d56c38eea2e6ff2076c97be20a92f7
Daniel Madrid <31113489+danimydev@users.noreply.github.com>
2023-07-03 18:02:20 -0500

    images router

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 import { Application, Router } from 'oak';
 import { ChampionsRepository } from './repository/champions.ts';
+import { ImageRepository } from './repository/images.ts';
 
 const championsRouter = new Router()
 	.get('/champions', (ctx) => {
@@ -12,6 +13,18 @@ const championsRouter = new Router()
 		return ctx.response.body = ChampionsRepository.getById(ctx.params.id);
 	});
 
+const imagesRouter = new Router()
+	.get('/images/:championName', (ctx) => {
+		const searchParams = ctx.request.url.searchParams;
+		return ctx.response.body = ImageRepository.getLoading(
+			{
+				championName: ctx.params.championName,
+				skin: searchParams.get('skin') || '',
+			},
+		);
+	});
+
 const app = new Application();
 app.use(championsRouter.routes());
+app.use(imagesRouter.routes());
 await app.listen({ port: 3000 });
--- src/repository/images.ts
+++ src/repository/images.ts
@@ -1,19 +1,23 @@
 export class ImageRepository {
-	static async getSplash(championName: string, skin: number) {
+	static async getSplash(
+		{ championName, skin }: { championName: string; skin: string },
+	) {
 		const url =
 			`http://ddragon.leagueoflegends.com/cdn/img/champion/splash/${championName}_${skin}.jpg`;
 		const res = await fetch(url);
 		return res;
 	}
 
-	static async getLoading(championName: string, skin: number) {
+	static async getLoading(
+		{ championName, skin }: { championName: string; skin: string },
+	) {
 		const url =
 			`http://ddragon.leagueoflegends.com/cdn/img/champion/loading/${championName}_${skin}.jpg`;
 		const res = await fetch(url);
 		return res;
 	}
 
-	static async getSquare(championName: string) {
+	static async getSquare({ championName }: { championName: string }) {
 		const url =
 			`http://ddragon.leagueoflegends.com/cdn/13.13.1/img/champion/${championName}.png`;
 		const res = await fetch(url);
