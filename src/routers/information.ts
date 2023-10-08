dfa10e791caab988ebe7e8d6faca73bf7bd3e2cc
Daniel Madrid <31113489+danimydev@users.noreply.github.com>
2023-10-07 22:05:08 -0500

    return versions on /versions

--- src/routers/information.ts
+++ src/routers/information.ts
@@ -1,13 +1,14 @@
 import { Router } from 'oak';
 
 import REGIONS from '@/data/regions.ts';
+import VERSIONS from '@/data/versions.ts';
 
 const informationRouter = new Router({ prefix: '/information' })
   .get('/regions', (ctx) => {
     return ctx.response.body = REGIONS;
   })
   .get('/versions', (ctx) => {
-    return ctx.response.body = [];
+    return ctx.response.body = VERSIONS;
   });
 
 export default informationRouter;
