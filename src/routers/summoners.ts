27ddcd143d60b1fe8ddb421fb41c7d6ad470cc91
Daniel Madrid <danimydev@gmail.com>
2024-02-18 10:04:53 -0500

    feat account riot

--- src/routers/summoners.ts
+++ src/routers/summoners.ts
@@ -3,6 +3,46 @@ import { Router } from "oak";
 import riotGames from "@/services/riot-games/index.ts";
 
 export const summonersRouter = new Router({ prefix: "/summoners" })
+  .get("/account", async (ctx) => {
+    try {
+      const { searchParams } = ctx.request.url;
+      const region = searchParams.get("region");
+      const apiKey = ctx.request.headers.get("api_key");
+      const gameName = searchParams.get("game_name");
+      const tagLine = searchParams.get("tag_line");
+
+      if (!gameName || !tagLine) {
+        ctx.response.status = 400;
+        return ctx.response.body = {
+          error: "gameName and tagLine are required",
+        };
+      }
+
+      if (!apiKey) {
+        ctx.response.status = 400;
+        return ctx.response.body = {
+          error: "api key not sent",
+        };
+      }
+
+      const account = await riotGames.getAccount({
+        gameName,
+        region,
+        apiKey,
+        tagLine,
+      });
+
+      ctx.response.status = 200;
+      return ctx.response.body = {
+        account,
+      };
+    } catch (error) {
+      ctx.response.status = 500;
+      return ctx.response.body = {
+        error,
+      };
+    }
+  })
   .get("/:summonerName", async (ctx) => {
     try {
       const { summonerName } = ctx.params;
@@ -17,11 +57,16 @@ export const summonersRouter = new Router({ prefix: "/summoners" })
         };
       }
 
-      return ctx.response.body = await riotGames.getSummoner({
+      const summoner = await riotGames.getSummoner({
         apiKey,
         region,
         summonerName,
       });
+
+      ctx.response.status = 200;
+      return ctx.response.body = {
+        summoner,
+      };
     } catch (error) {
       ctx.response.status = 500;
       return ctx.response.body = {
--- src/services/riot-games/index.ts
+++ src/services/riot-games/index.ts
@@ -1,6 +1,10 @@
-import { SummonersByNameEndpointResponse } from "@/services/riot-games/types.ts";
+import {
+  GetAccountEndpointResponse,
+  GetSummonersByNameEndpointResponse,
+} from "@/services/riot-games/types.ts";
 
 const DEFAULT_REGION = "LA1";
+const DEFAULT_ACCOUNT_REGION = "americas";
 
 const getSummoner = async (args: {
   summonerName: string;
@@ -17,12 +21,17 @@ const getSummoner = async (args: {
 
   const response = await fetch(url);
 
+  if (!response.ok) {
+    throw new Error("response not ok from riot games services");
+  }
+
   const { status, statusText } = response;
   if (status !== 200) {
     throw new Error(statusText);
   }
 
-  const summoner = await response.json() as SummonersByNameEndpointResponse;
+  const summoner = await response.json() as GetSummonersByNameEndpointResponse;
+
   if (!summoner) {
     throw new Error("summoner not found in riotgames response payload");
   }
@@ -30,4 +39,41 @@ const getSummoner = async (args: {
   return summoner;
 };
 
-export default { getSummoner };
+const getAccount = async (args: {
+  gameName: string;
+  tagLine: string;
+  apiKey: string;
+  region?: string | null;
+}) => {
+  const region = args.region || DEFAULT_ACCOUNT_REGION;
+
+  const response = await fetch(
+    `https://${region}.api.riotgames.com/riot/account/v1/accounts/by-riot-id/${
+      encodeURIComponent(args.gameName)
+    }/${encodeURIComponent(args.tagLine)}`,
+    {
+      method: "GET",
+      headers: {
+        "Accept-Language": "en-US,en;q=0.9",
+        "Accept-Charset": "application/x-www-form-urlencoded; charset=UTF-8",
+        "X-Riot-Token": args.apiKey,
+      },
+    },
+  );
+
+  if (!response.ok) {
+    throw new Error("response not ok from riot games services");
+  }
+
+  const { status, statusText } = response;
+  if (status !== 200) {
+    throw new Error(statusText);
+  }
+
+  return await response.json() as GetAccountEndpointResponse;
+};
+
+export default {
+  getSummoner,
+  getAccount,
+};
